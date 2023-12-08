77072923e4aaff56cb7f79e7ac706a8277e8ff8e
Nick <93751758+kofflernick@users.noreply.github.com>
2023-12-08 10:59:23 -0500

    preliminary work on coding for tabs 1 and 2

--- app/(tabs)/one.tsx
+++ app/(tabs)/one.tsx
@@ -1,14 +1,34 @@
 import React from "react"
-import { Image, StyleSheet } from "react-native"
-import { View } from "../../components/Themed"
+import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native"
 
 export default function TabOneScreen() {
+  const handleTap = () => {
+    //future implementation
+  }
+
   return (
     <View style={styles.container}>
-      <Image
-        source={require("../figma_statics/Home.jpg")}
-        style={styles.image}
-      />
+      <Text style={styles.greeting}>Hello Toma,</Text>
+      <Text style={styles.message}>
+        Your next scheduled workout is today at 1:00 PM EST @ Dodge (Columbia)
+      </Text>
+      <View style={styles.buddySection}>
+        <Text style={styles.buddyTitle}>Your Buddy</Text>
+        <Image
+          source={require("../cat/HomeYourBuddy.jpg")}
+          style={styles.buddyImage}
+        />
+        <Text style={styles.time}>01:00 - 02:00 PM</Text>
+        <Text style={styles.streak}>Streak x10</Text>
+      </View>
+      <TouchableOpacity style={styles.nearbySection} onPress={handleTap}>
+        <Text style={styles.nearbyTitle}>Buddies Nearby</Text>
+        <Image
+          source={require("../cat/HomeBuddiesNearby.jpg")}
+          style={styles.nearbyImage}
+        />
+        <Text style={styles.tapAction}>Tap To See Buddies Near You</Text>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -16,12 +36,65 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 20,
+    backgroundColor: "#1C1C1E",
   },
-  image: {
-    width: "100%",
-    height: "100%",
-    resizeMode: "contain",
+  greeting: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "#D9D9D9",
+  },
+  message: {
+    fontSize: 18,
+    color: "#D9D9D9",
+    marginVertical: 10,
+  },
+  buddySection: {
+    alignItems: "center",
+    backgroundColor: "#4e9525",
+    padding: 10,
+    borderRadius: 10,
+    marginVertical: 10,
+  },
+  buddyTitle: {
+    fontSize: 24,
+    color: "#D9D9D9",
+    marginBottom: 10,
+  },
+  buddyImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 10,
+  },
+  time: {
+    fontSize: 18,
+    color: "#D9D9D9",
+  },
+  streak: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#ffd700",
+  },
+  nearbySection: {
+    alignItems: "center",
+    padding: 10,
+    marginVertical: 10,
+  },
+  nearbyTitle: {
+    fontSize: 24,
+    color: "#D9D9D9",
+    marginBottom: 10,
+  },
+  nearbyImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 10,
+  },
+  tapAction: {
+    fontSize: 18,
+    color: "#D9D9D9",
+    textDecorationLine: "underline",
   },
 })
--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,13 +1,61 @@
 import React from "react"
-import { Image, StyleSheet } from "react-native"
-import { View } from "../../components/Themed"
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  FlatList,
+  StyleSheet,
+  ListRenderItemInfo,
+} from "react-native"
+
+type ChatItem = {
+  id: string
+  name: string
+  message: string
+  image: any
+}
+
+//data for the chats
+const chats: ChatItem[] = [
+  {
+    id: "1",
+    name: "Lisa",
+    message: "Are you free this friday?",
+    image: require("../cat/HomeBuddiesNearby.jpg"),
+  },
+  {
+    id: "2",
+    name: "Xiaofeng",
+    message: "I dont like Equinox",
+    image: require("../cat/HomeBuddiesNearby.jpg"),
+  },
+  // other chats to be added
+]
 
 export default function TabTwoScreen() {
+  const renderItem = ({ item }: ListRenderItemInfo<ChatItem>) => (
+    <TouchableOpacity style={styles.chatItem}>
+      <Image source={item.image} style={styles.chatImage} />
+      <View style={styles.chatContent}>
+        <Text style={styles.chatName}>{item.name}</Text>
+        <Text style={styles.chatMessage}>{item.message}</Text>
+      </View>
+      {/* Replace with the correct "chevron" */}
+      <Image
+        source={require("../cat/HomeBuddiesNearby.jpg")}
+        style={styles.chevron}
+      />
+    </TouchableOpacity>
+  )
+
   return (
     <View style={styles.container}>
-      <Image
-        source={require("../figma_statics/Chats.jpg")}
-        style={styles.image}
+      <Text style={styles.header}>Chats</Text>
+      <FlatList
+        data={chats}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
       />
     </View>
   )
@@ -16,12 +64,44 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#1C1C1E",
   },
-  image: {
-    width: "100%",
-    height: "100%",
-    resizeMode: "contain",
+  header: {
+    fontSize: 34,
+    color: "#fff",
+    fontWeight: "bold",
+    padding: 10,
+  },
+  chatItem: {
+    flexDirection: "row",
+    padding: 10,
+    backgroundColor: "#2C2C2E",
+    borderRadius: 10,
+    marginVertical: 5,
+    marginHorizontal: 10,
+    alignItems: "center",
+  },
+  chatImage: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+  },
+  chatContent: {
+    marginLeft: 10,
+    flex: 1,
+  },
+  chatName: {
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  chatMessage: {
+    fontSize: 16,
+    color: "#ddd",
+  },
+  chevron: {
+    width: 10,
+    height: 10,
+    tintColor: "#fff",
   },
 })
